3e2302696b15306870f767f761ae660f3ec4fc1d
Alakbarbh <alakbarbh@code.edu.az>
2023-04-11 11:16:35 -0800

    Asp.net practices, Elearn template

---
+++ EducationSayt/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using EducationSayt.Data;
+using EducationSayt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationSayt.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Course> courses = await _context.Courses.Include(c=>c.CourseImages).Include(c=>c.Author).Where(m => !m.SoftDelete).ToListAsync();
+            return View(courses);
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return BadRequest();
+            Course course = await _context.Courses.Include(c=>c.CourseImages).Include(c=>c.Author).FirstOrDefaultAsync(m => m.Id == id);
+            if (course is null) return NotFound();
+            return View(course);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ EducationSayt/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace EducationSayt.Models
+{
+    public class Course : BaseEntity 
+    {
+        public string? Price { get; set; }
+        public string? Header { get; set; }
+        public string? Description { get; set; }
+        public string? Sales { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public ICollection<CourseImage> CourseImages { get; set; }
+    }
+}
